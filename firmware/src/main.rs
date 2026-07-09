eb041fc48dfdd3ee3d08689287051dfa97b59dd1
Szybet <53944559+Szybet@users.noreply.github.com>
2026-07-09 16:44:29 +0200

    remove blink

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -3,35 +3,21 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
-
-#[embassy_executor::task]
-async fn blink_task(mut led: Output<'static>) -> ! {
-    loop {
-        led.toggle();
-        info!("LED toggled");
-        Timer::after(Duration::from_millis(500)).await;
-    }
-}
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let mut config = embassy_nrf::config::Config::default();
     config.lfclk_source = embassy_nrf::config::LfclkSource::ExternalXtal;
-    config.hfclk_source = embassy_nrf::config::HfclkSource::ExternalXtal;
-    // config.dcdc.reg1 = true;
-    
+    // config.hfclk_source = embassy_nrf::config::HfclkSource::ExternalXtal; // Don't, we don't have the 30Mhz Xtal
+    // config.dcdc.reg1 = true; // Try this later for radio optimisations
     let p = embassy_nrf::init(config);
-    info!("Blinky starting on nRF52805");
-
-    // let led = Output::new(p.P0_17, Level::High, OutputDrive::Standard);
-
-    // spawner.spawn(blink_task(led).unwrap());
+    
+    info!("Starting");
 
     loop {
-        info!("Fucking work");
-        Timer::after(Duration::from_secs(1)).await;
+        info!("Tick");
+        Timer::after(Duration::from_secs(3)).await;
     }
 }
