4026194560957d8777a807d079849d661d915441
Szybet <53944559+Szybet@users.noreply.github.com>
2026-07-15 21:39:08 +0200

    button working

--- firmware/tests/button/src/main.rs
+++ firmware/tests/button/src/main.rs
@@ -4,6 +4,7 @@
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_nrf::gpio::{Input, Pull};
+use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
@@ -12,10 +13,14 @@ async fn main(_spawner: Spawner) {
     config.lfclk_source = embassy_nrf::config::LfclkSource::ExternalXtal;
     let p = embassy_nrf::init(config);
 
-    let mut button = Input::new(p.P0_16, Pull::None);
-
     info!("Button test on P0.16");
     info!("Waiting for button press");
+
+    Timer::after_secs(20).await;
+
+    let mut button = Input::new(p.P0_16, Pull::Down); // Needs a pull down! and without external resistor
+
+    // Even tho it's interrupt, it's highly inneficient
 
     loop {
         button.wait_for_low().await;
