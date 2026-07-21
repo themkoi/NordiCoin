b29b0638d6eb87b10a3bc5d58e3a118a1e34d957
Szybet <53944559+Szybet@users.noreply.github.com>
2026-07-21 17:12:19 +0200

    cleaning & hive storage

---
+++ app/lib/data.dart
@@ -0,0 +1,132 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+part 'data.g.dart';
+
+@HiveType(typeId: 0)
+enum ActionType {
+  @HiveField(0)
+  none,
+
+  @HiveField(1)
+  notification,
+
+  @HiveField(2)
+  buzzing,
+
+  @HiveField(3)
+  loudAlarm,
+}
+
+@HiveType(typeId: 1)
+class OnAppDevice extends HiveObject {
+  @HiveField(0)
+  late int loudBuzzingTimeS;
+
+  @HiveField(1)
+  late int silentBuzzingTimeS;
+
+  @HiveField(2)
+  late int txPower;
+
+  @HiveField(3)
+  late int turningOffAlertTimeM;
+
+  @HiveField(4)
+  late int lowAlertLostDeviceTimeM;
+
+  @HiveField(5)
+  late int mediumAlertLostDeviceTimeM;
+
+  @HiveField(6)
+  late int highAlertLostDeviceTimeM;
+
+  @HiveField(7)
+  late ActionType lowAlertAction;
+
+  @HiveField(8)
+  late ActionType mediumAlertAction;
+
+  @HiveField(9)
+  late ActionType highAlertAction;
+
+  OnAppDevice({
+    this.loudBuzzingTimeS = 0,
+    this.silentBuzzingTimeS = 0,
+    this.txPower = 0,
+    this.turningOffAlertTimeM = 15,
+    this.lowAlertLostDeviceTimeM = 5,
+    this.mediumAlertLostDeviceTimeM = 10,
+    this.highAlertLostDeviceTimeM = 15,
+    this.lowAlertAction = ActionType.none,
+    this.mediumAlertAction = ActionType.none,
+    this.highAlertAction = ActionType.none,
+  });
+}
+
+@HiveType(typeId: 2)
+class Device extends HiveObject {
+  @HiveField(0)
+  late String aliasName;
+
+  @HiveField(1)
+  late String macAddress;
+
+  @HiveField(2)
+  late double batteryVoltage;
+
+  @HiveField(3)
+  late DateTime lastSeenTime;
+
+  @HiveField(4)
+  late int lastSeenRssi;
+
+  @HiveField(5)
+  late String id;
+
+  @HiveField(6)
+  late OnAppDevice deviceSettings;
+
+  Device({
+    this.aliasName = '',
+    this.macAddress = '',
+    this.batteryVoltage = 3.3,
+    required this.lastSeenTime,
+    this.lastSeenRssi = 0,
+    this.id = '',
+    required this.deviceSettings,
+  });
+}
+
+@HiveType(typeId: 3)
+class Settings extends HiveObject {
+  @HiveField(0)
+  late bool alertsManualOverride;
+
+  @HiveField(1)
+  late int alertsOffAfterTimeH;
+
+  @HiveField(2)
+  late int alertsOffAfterTimeM;
+
+  @HiveField(3)
+  late int alertsOnAfterTimeH;
+
+  @HiveField(4)
+  late int alertsOnAfterTimeM;
+
+  @HiveField(5)
+  late int scanFrequencyTimeM;
+
+  @HiveField(6)
+  late OnAppDevice defaultDeviceSettings;
+
+  Settings({
+    this.alertsManualOverride = false,
+    this.alertsOffAfterTimeH = 0,
+    this.alertsOffAfterTimeM = 0,
+    this.alertsOnAfterTimeH = 0,
+    this.alertsOnAfterTimeM = 0,
+    this.scanFrequencyTimeM = 2,
+    required this.defaultDeviceSettings,
+  });
+}
---
+++ app/lib/data.g.dart
@@ -0,0 +1,221 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'data.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class OnAppDeviceAdapter extends TypeAdapter<OnAppDevice> {
+  @override
+  final int typeId = 1;
+
+  @override
+  OnAppDevice read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return OnAppDevice(
+      loudBuzzingTimeS: fields[0] as int,
+      silentBuzzingTimeS: fields[1] as int,
+      txPower: fields[2] as int,
+      turningOffAlertTimeM: fields[3] as int,
+      lowAlertLostDeviceTimeM: fields[4] as int,
+      mediumAlertLostDeviceTimeM: fields[5] as int,
+      highAlertLostDeviceTimeM: fields[6] as int,
+      lowAlertAction: fields[7] as ActionType,
+      mediumAlertAction: fields[8] as ActionType,
+      highAlertAction: fields[9] as ActionType,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, OnAppDevice obj) {
+    writer
+      ..writeByte(10)
+      ..writeByte(0)
+      ..write(obj.loudBuzzingTimeS)
+      ..writeByte(1)
+      ..write(obj.silentBuzzingTimeS)
+      ..writeByte(2)
+      ..write(obj.txPower)
+      ..writeByte(3)
+      ..write(obj.turningOffAlertTimeM)
+      ..writeByte(4)
+      ..write(obj.lowAlertLostDeviceTimeM)
+      ..writeByte(5)
+      ..write(obj.mediumAlertLostDeviceTimeM)
+      ..writeByte(6)
+      ..write(obj.highAlertLostDeviceTimeM)
+      ..writeByte(7)
+      ..write(obj.lowAlertAction)
+      ..writeByte(8)
+      ..write(obj.mediumAlertAction)
+      ..writeByte(9)
+      ..write(obj.highAlertAction);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is OnAppDeviceAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class DeviceAdapter extends TypeAdapter<Device> {
+  @override
+  final int typeId = 2;
+
+  @override
+  Device read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Device(
+      aliasName: fields[0] as String,
+      macAddress: fields[1] as String,
+      batteryVoltage: fields[2] as double,
+      lastSeenTime: fields[3] as DateTime,
+      lastSeenRssi: fields[4] as int,
+      id: fields[5] as String,
+      deviceSettings: fields[6] as OnAppDevice,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Device obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.aliasName)
+      ..writeByte(1)
+      ..write(obj.macAddress)
+      ..writeByte(2)
+      ..write(obj.batteryVoltage)
+      ..writeByte(3)
+      ..write(obj.lastSeenTime)
+      ..writeByte(4)
+      ..write(obj.lastSeenRssi)
+      ..writeByte(5)
+      ..write(obj.id)
+      ..writeByte(6)
+      ..write(obj.deviceSettings);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DeviceAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class SettingsAdapter extends TypeAdapter<Settings> {
+  @override
+  final int typeId = 3;
+
+  @override
+  Settings read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Settings(
+      alertsManualOverride: fields[0] as bool,
+      alertsOffAfterTimeH: fields[1] as int,
+      alertsOffAfterTimeM: fields[2] as int,
+      alertsOnAfterTimeH: fields[3] as int,
+      alertsOnAfterTimeM: fields[4] as int,
+      scanFrequencyTimeM: fields[5] as int,
+      defaultDeviceSettings: fields[6] as OnAppDevice,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Settings obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.alertsManualOverride)
+      ..writeByte(1)
+      ..write(obj.alertsOffAfterTimeH)
+      ..writeByte(2)
+      ..write(obj.alertsOffAfterTimeM)
+      ..writeByte(3)
+      ..write(obj.alertsOnAfterTimeH)
+      ..writeByte(4)
+      ..write(obj.alertsOnAfterTimeM)
+      ..writeByte(5)
+      ..write(obj.scanFrequencyTimeM)
+      ..writeByte(6)
+      ..write(obj.defaultDeviceSettings);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SettingsAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class ActionTypeAdapter extends TypeAdapter<ActionType> {
+  @override
+  final int typeId = 0;
+
+  @override
+  ActionType read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return ActionType.none;
+      case 1:
+        return ActionType.notification;
+      case 2:
+        return ActionType.buzzing;
+      case 3:
+        return ActionType.loudAlarm;
+      default:
+        return ActionType.none;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, ActionType obj) {
+    switch (obj) {
+      case ActionType.none:
+        writer.writeByte(0);
+        break;
+      case ActionType.notification:
+        writer.writeByte(1);
+        break;
+      case ActionType.buzzing:
+        writer.writeByte(2);
+        break;
+      case ActionType.loudAlarm:
+        writer.writeByte(3);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ActionTypeAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
