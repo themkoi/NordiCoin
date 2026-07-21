70b2c1b800105a2e921fb115dc434ef6f2b195fd
Szybet <53944559+Szybet@users.noreply.github.com>
2026-07-21 17:56:09 +0200

    change app codename

--- app/android/app/src/main/kotlin/com/szybet/nordicoin/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/szybet/nordicoin/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.nordicoin
+package com.szybet.nordicoin
 
 import io.flutter.embedding.android.FlutterActivity
 
--- app/scripts/install.sh
+++ app/scripts/install.sh
@@ -4,4 +4,4 @@ adb install build/app/outputs/flutter-apk/app-release.apk
 sleep 1
 killall -9 adb
 sleep 1
-adb shell am start -n com.example.nordicoin/.MainActivity
+adb shell am start -n com.szybet.nordicoin/.MainActivity
